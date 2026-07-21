71d001bd66504b4b0868652a44ecd4434c815d90
Vincent Chang <zvincentchang@seed.net.tw>
2026-07-21 18:37:45 +0800

    Delete Unit4 ~5 Codes

--- notes/Unit04-nested-loops/TypeConvert.java
+++
@@ -1,9 +0,0 @@
-public class TypeConvert {
-    public static void main(String[] args){
-        double d1=3.6;
-        int v1=(int)d1;
-        System.out.println("d1: " + d1);
-        System.out.println("v1: " + v1);
-
-    }
-}
--- notes/Unit06-loop-math/Loop1.java
+++
@@ -1,9 +0,0 @@
-public class Loop1 {
-    public static void main(String[] args) {
-        int x = 0;
-        do {
-            System.out.print(x + " ");
-        } while (++x < 5);
-        System.out.print(x);
-    }
-}
